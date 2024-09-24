41206cb8369ae273a76d6e3602102097428dee70
manishjadhav06 <manishjadhav.occ@gmail.com>
2024-09-24 21:38:19 +0530

    pushing first

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [num, setNum] = useState(false)
   const [char, setChar] = useState(false)
   const [passw, setPassw] = useState("")
+  const [color, setColor] = useState("olive")
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -40,6 +41,29 @@ function App() {
           <label htmlFor="">Character</label>           
           </div>
         </div>
+      </div>
+       {/*----------- Background Changer--------------- */}
+
+       <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+      </div>
       </div>
     </>
   )
